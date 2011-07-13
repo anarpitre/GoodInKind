408b8ab039f756023db1dd0506f9d8a754afe9cd
Mehul Gurjar <mehul@joshsoftware.com>
2011-07-13 15:36:12 +0530

    + Config folder Added

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Goodinkind::Application.config.secret_token = '0d8d1bc9965cf781d351de4242d0ae86c70fa1d541fdc4055b4878e0f7e2e05d22b7cd6e24a523a3061de702fdedb3ca682087888b9cdf77bb5637af34830c5a'
