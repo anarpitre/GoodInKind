914a0b142fb186fb38c29f225abcd938b0c70d7d
Sameer-Tilak <sameert@joshsoftware.com>
2011-07-13 19:48:55 +0530

    Modified model category, location, profile

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,6 @@
 class Category < ActiveRecord::Base
 
-  has_many :non_profit_categories
+  has_many :non_profit_categories, :dependent => :destory
   has_many :non_profits, :through => :non_profit_categories
 
   has_many :service_categories, :dependent => :destroy
