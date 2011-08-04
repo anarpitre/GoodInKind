47f2e7c5927bc53fa0d0b6f2ac46f83449339e2c
Mehul Gurjar <mehul@joshsoftware.com>
2011-08-04 15:15:58 +0530

    +Service & NonProfit Categories Defined in Seed.rb file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
-
+# NonProfits Categories +++++++++++++++++++++
 a = ["A", "B", "C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","z","W", "Y" ,"Z"] 
  b = [ "Arts, Culture & Humanities" ,"Education" ,"Environment", "Animal-Related" ," Health Care","Mental Health & Crisis Intervention" ,"Voluntary Health Associations & Medical Disciplines" ,"Medical Research" ," Crime & Legal-Related" ,"Employment" ,"Food, Agriculture, Nutrition" ,"Housing & Shelter" ,"Public Safety, Disaster Preparedness & Relief" ,"Recreation & Sports" ,"Youth Development","Human Services","International, Foreign Affairs" ,"Civil Rights, Social Action & Advocacy" ,"Community Improvement & Capacity Building" ,"Philanthropy, Voluntarism & Grantmaking Foundations","Science & Technology","Social Science","Public & Societal Benefit","Religion-Related", "Mutual & Membership Benefit" ,"Unknown"]
  x ={}
@@ -13,4 +13,14 @@ a = ["A", "B", "C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","
    c = c + 1
    cat.save
 end
+#++++++++++++++++++++++++
 
+# Service Categories
+
+category = ["Arts and crafts", "Beauty and style", "Computer and Technology", "Education and literature", "Family", "Food and drinks", "Health and wellbeing", "History and Culture", "Home and garden", "Money and career", "Nightlife", "Recreation and outdoors", "Sports", "Tours"] 
+category.each do |cat|
+  a = Category.new
+  a.name = cat
+  a.category_type = 1
+  a.save
+end
