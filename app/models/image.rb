aba591d77b8f7c0a26788d09f1a7f3d672c4df60
pratik <pratik@joshsoftware.com>
2011-08-08 20:53:56 +0530

    Code cleanup
    Image is saved on local for development and for production its is saved
    on cloud server

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,18 +1,20 @@
 class Image < ActiveRecord::Base
   belongs_to :service
-  
-  has_attached_file :image,
-    :styles => { :small_thumb => [ "50x50!", :jpg ],
-      :medium_thumb => [ "75x75!", :jpg ],
-      :carousal => [ "81x81!", :jpg ],
-      :large_thumb => [ "120x120>", :jpg ],
-      :detail_preview => [ "210x210!", :jpg ]} 
-     # :storage => :s3,
-      #:s3_credentials => "#{RAILS_ROOT}/config/s3.yml",
-      #:path => ":attachment/:id/:style.:extension",
-      #:bucket => 'yourbucket'#size set according to logo size available in xhtmls(brands_review_detail.html)
 
-      #:default_url => "/images/missing/logo/:style.gif"
-  #validates_attachment_content_type :logo, :content_type => ['image/pjpeg','image/jpeg', 'image/png','image/x-png','image/gif'], :message=>:logo_format
-  #validates_attachment_size :logo, :less_than => 5.megabytes, :message => "Uploaded logo cannot be more than 5 MB"
+  if RAILS_ENV == production
+    has_attached_file :image,
+      :styles => { :thumb => [ "200x100#", :jpg ], :detail => [ "448x260>", :jpg ]  
+      :storage => :s3,
+      :s3_credentials => "#{RAILS_ROOT}/config/s3.yml",
+      :path => ":attachment/:id/:style.:extension",
+      :bucket => 'yourbucket',                       
+      :default_url => "/images/missing/logo/:style.gif"}
+  else
+    has_attached_file :image,
+      :styles => { :thumb => [ "200x100#", :jpg ], :detail => [ "448x260>", :jpg ] } 
+  end
+
+  validates_attachment_content_type :image, :content_type => ['image/pjpeg','image/jpeg', 'image/png','image/x-png','image/gif'], :message=>:logo_format
+  validates_attachment_size :image, :less_than => 5.megabytes, :message => "Uploaded logo cannot be more than 5 MB"
+
 end
