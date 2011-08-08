ab95cd2346adb986d6816fcc1fcc23c44bd0106e
Gautam Rege <gautam@joshsoftware.com>
2011-08-08 21:39:08 +0530

    Fixing syntax error!!

--- config/initializers/gik.rb
+++ config/initializers/gik.rb
@@ -1,10 +1,10 @@
-if RAILS_ENV == production
+if RAILS_ENV == 'production'
   S3_DEFAULTS = { 
       :storage => :s3,
       :s3_credentials => "#{RAILS_ROOT}/config/s3.yml",
       :path => ":attachment/:id/:style.:extension",
       :bucket => 'yourbucket',                       
-      :default_url => "/images/missing/logo/:style.gif"}
+      :default_url => "/images/missing/logo/:style.gif"
   }
 else
   S3_DEFAULTS = { }
