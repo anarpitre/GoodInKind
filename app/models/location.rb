207f1d60aba583aab84aec9a59ba433839f3d4f0
pratik <pratik@joshsoftware.com>
2011-08-08 20:55:29 +0530

    Code cleanup:  Change in image size

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,7 +1,7 @@
 class Location < ActiveRecord::Base
   belongs_to :resource, :polymorphic => true
 
-  validates :address,:latitude,:longitude,:presence => true  
+  validates :address,:latitude,:longitude,:presence => true, :message => "dd" 
 
   before_validation :full_address
 
@@ -11,14 +11,7 @@ class Location < ActiveRecord::Base
     if geo && geo.first 
       geo = geo.first
       self.city = geo.city
-      #State name is under the name of administrative_area_level_1 in hash result
-      count = 0
-      geo.data["address_components"].size.times do 
-        if geo.data["address_components"][count]["types"][0] == "administrative_area_level_1"
-          self.state =  geo.data["address_components"][count]["short_name"]
-        end
-        count += 1
-      end
+      self.state = geo.state
       self.country = geo.country_code
       self.address= geo.formatted_address
       self.zip = geo.postal_code
