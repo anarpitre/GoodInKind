1d7a0e596519b11e9fe78869f89b5b94e473c3d8
Mehul Gurjar <mehul@joshsoftware.com>
2011-08-09 12:36:21 +0530

    + Home page(Index) Featured service Added + Offerer Form Design

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,10 @@ class HomeController < ApplicationController
   before_filter :set_seo_tags
   layout 'home'
 
+  def index
+    @services = Service.all
+  end
+
   def set_seo_tags
     @head = {
       :title => "Home",
