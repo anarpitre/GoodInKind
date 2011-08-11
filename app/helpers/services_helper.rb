4fcaa710fc58cc5e5e767ba13c4b26bf8b3c1cc0
pratik <pratik@joshsoftware.com>
2011-08-11 16:49:04 +0530

    Helper method to get non profit name

--- app/helpers/services_helper.rb
+++ app/helpers/services_helper.rb
@@ -1,6 +1,9 @@
 module ServicesHelper
-  def get_non_profit_name(id)
-    return nil if id.to_i == 0
-    return NonProfit.find(id).name
+  def get_non_profit_name
+    return @service.non_profit.blank? ? "" : @service.non_profit.name
+  end
+
+  def get_remaining_spots
+    return (@service.booking_capacity.to_i - @service.booked_seats.to_i).to_i
   end
 end
