1fda19e71bf7a68ccd92034935b07b764c5e190c
Amit Kulkarni <amit@joshsoftware.com>
2011-08-17 19:46:21 +0530

    updated

--- spec/models/nonprofit_spec.rb
+++ spec/models/nonprofit_spec.rb
@@ -2,18 +2,18 @@ require 'spec_helper'
 require 'faker'
 
 describe Nonprofit do
-  
+
   context "should be created if" do
-  let(:np) {Factory(:nonprofit) }
-  after(:each) do
-    np.save
-    np.should be_valid
-  end
+    let(:np) {Factory(:nonprofit) }
+    after(:each) do
+      np.save
+      np.should be_valid
+    end
 
     it "all the details entered are valid" do
       np.should be_valid
     end
-    
+
     it "Ein is blank" do
       np.EIN = ''
     end
@@ -42,12 +42,10 @@ describe Nonprofit do
   end
 
   context "should not be created if" do
-  let(:np) {Factory(:nonprofit) }
-   after(:each) do
+    let(:np) {Factory(:nonprofit) }
+    after(:each) do
       np.save
-      p np.password
-      p np.password_confirmation
-     np.should_not be_valid
+      np.should_not be_valid
     end
 
     it "Ein is already taken"
@@ -60,16 +58,16 @@ describe Nonprofit do
     it "website is not in correct format i.e. google" do
       np.website = 'google'
     end
-    
+
     it "username is blank" do
       np.username = ''
     end
-    
+
     it "username is already taken" do
       non1 = Factory(:nonprofit)
       np.username = non1.username
     end
-    
+
     it "confirm password is not matching with password" do
       np.password = 'josh123'
       np.password_confirmation = '123josh'
@@ -96,7 +94,7 @@ describe Nonprofit do
       np.photo_file_name = ''
       np.should_not be_valid
     end
-    
+
     it "if uuid is  blank"
     it "if permalink is not set"
     it "if uploaded photo is not in proper format e.g. txt"
