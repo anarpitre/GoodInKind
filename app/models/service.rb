3e3a37346a652e8b1d5b036c53e6e84fa15f5f4f
pratik <pratik@joshsoftware.com>
2011-08-17 20:20:59 +0530

    Added column permalink

--- app/models/service.rb
+++ app/models/service.rb
@@ -20,6 +20,8 @@ class Service < ActiveRecord::Base
   validates :start_date, :end_date, :start_time, :end_time, :if => Proc.new { |t| t.is_schedulelater == false}, :presence => true
   validate :check_categories
   validate :check_date
+  
+  after_create :generate_permalink
 
   aasm_column :status
   aasm_initial_state :pending
@@ -32,7 +34,7 @@ class Service < ActiveRecord::Base
 
 
   def to_param
-    "#{id}-#{title.parameterize}"
+    permalink || "#{id}-#{title.parameterize}"
   end
 
   def check_categories
@@ -44,6 +46,11 @@ class Service < ActiveRecord::Base
       errors.add(:start_date,"Check Date") unless (self.start_date || self.end_date || (self.start_date < self.end_date))
       errors.add(:start_time," Check time") unless (self.start_time || self.end_time || (self.start_time <= self.end_time))
     end
+  end
+
+
+  def generate_permalink
+    update_attribute(:permalink ,self.to_param)
   end
 
 end
