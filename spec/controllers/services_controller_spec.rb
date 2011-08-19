990f1c82018954f49b7d507c1ed8f2cf2383296e
Amit Kulkarni <amit@joshsoftware.com>
2011-08-19 18:22:50 +0530

    Updated services controller

--- spec/controllers/services_controller_spec.rb
+++ spec/controllers/services_controller_spec.rb
@@ -7,43 +7,45 @@ describe ServicesController do
     sign_in @user
   end
   context "User should be able to" do
-
-  # This should return the minimal set of attributes required to create a valid
-  # Service. As you add validations to Service, be sure to
-  # update the return value of this method accordingly.
-  def valid_attributes
-    {
-      :title => "MyString",
-      :description => "MyString",
-      :amount => 1.5,
-      :booking_capacity => 1,
-      :is_schedulelater => true,
-      :nonprofit_id => 1,
-      :is_virtual => false,
-      :is_public => true
-    }
-  end
-
-  describe "GET index" do
-    it "create a service"
-
-    it "assigns all services as @services" do
-      service = Service.create! valid_attributes
-      get :index
-      assigns(:services).should_not eq([service])
+    it "create a service with single category" do
+      cat = Category.all.map(&:id)
+      non_p = Factory(:nonprofit)
+      post :create, :nonprofit_name => non_p.name,
+                    :service => {:is_schedulelater => true, :title => "service-create", :is_virtual => true, :booking_capacity => 25, :estimated_duration => 45, :amount => 200, :description => "testing of creation of service", :is_public => true, :category_ids => ["#{cat[0]}"],:nonprofit_id => non_p.id}
+      ser = Service.find_by_title("service-create")
+      ser.should_not == nil
     end
-  end
-
-  describe "GET show" do
-    it "assigns the requested service as @service" do
-      service = Service.create! valid_attributes
-      get :show, :id => service.id.to_s
-      assigns(:service).should eq(service)
+    
+    it "create a service with multiple categories" do
+      cat = Category.all.map(&:id)
+      non_p = Factory(:nonprofit)
+      post :create, :nonprofit_name => non_p.name,
+                    :service => {:is_schedulelater => true, :title => "service-create-with-multiple-category", :is_virtual => true, :booking_capacity => 25, :estimated_duration => 45, :amount => 200, :description => "testing of creation of service", :is_public => true, :category_ids => ["#{cat[0]}, #{cat[1]}, #{cat[2]}"],:nonprofit_id => non_p.id}
+      ser = Service.find_by_title("service-create-with-multiple-category")
+      ser.should_not == nil
     end
-  end
-    it "create a service with same non-profit"
 
-    it "update a service"
+    it "create a service with same non-profit" do
+      cat = Category.all.map(&:id)
+      ser = Factory(:service)
+      post :create, :nonprofit_name => ser.nonprofit.name,
+                    :service => {:is_schedulelater => true, :title => "service-with-same-nonp", :is_virtual => true, :booking_capacity => 25, :estimated_duration => 45, :amount => 200, :description => "testing of creation of service", :is_public => true, :category_ids => ["#{cat[0]}, #{cat[1]}, #{cat[2]}"],:nonprofit_id => ser.nonprofit.id}
+      ser1 = Service.find_by_title("service-with-same-nonp")
+      ser1.should_not == nil
+      ser.nonprofit.name.should == ser1.nonprofit.name
+    end
+
+    it "update a service" do
+      cat = Category.all.map(&:id)
+      ser = Factory(:service)
+      post :update, :nonprofit_name => ser.nonprofit.name,
+                    :service => { :title => "ser_update"},
+                    :id => ser.permalink
+      ser_old = Service.find_by_title(ser.title)
+      ser_old.should == nil
+      ser_new = Service.find_by_title("ser_update")
+      ser_new.should_not == nil
+    end
 
     it "delete a service"
   end
