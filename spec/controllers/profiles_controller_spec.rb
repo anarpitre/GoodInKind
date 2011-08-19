4de2c493dbf7925f3f60db5b27551add7c219add
Amit Kulkarni <amit@joshsoftware.com>
2011-08-19 18:22:34 +0530

    Updated profiles controller

--- spec/controllers/profiles_controller_spec.rb
+++ spec/controllers/profiles_controller_spec.rb
@@ -7,11 +7,25 @@ describe ProfilesController do
     sign_in @user
   end
 
-  context "User profile should be created" do
+  context "User profile should be updated" do
     it "by post request" do
-    post :create, :profile => {:about_me => "This is controller testing of profile", :gender => "male", :last_name => "kulkarni", :first_name => "amit"}
-    @user.profile.should_not == nil
-    @user.profile.about_me.should == "This is controller testing of profile"
+      post :update, :profile => {:about_me => "This is controller testing of profile", :gender => "male", :last_name => "kulkarni", :first_name => "amit"},
+                    :id => @user.id  
+      user = User.find(@user)
+      user.profile.should_not == nil
+      user.profile.about_me.should == "This is controller testing of profile"
     end
+
+    it "with address using post request" do
+      post :update, :profile => {:about_me => "This is location testing of profile", :gender => "male",
+                    :location_attributes => {:address => "pune"}},
+                    :id => @user.id  
+      user = User.find(@user)
+      user.profile.should_not == nil
+      user.profile.about_me.should == "This is location testing of profile"
+      user.profile.location.address.should == "Pune, Maharashtra, India"
+    end
+
+    it "by his image using post request"
   end
 end
