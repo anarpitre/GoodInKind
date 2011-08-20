58abbb539420e4ab4f50a618891a35ec04232fba
Sameer-Tilak <sameert@joshsoftware.com>
2011-08-20 16:37:58 +0530

    debug in location error in prfile edit

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -15,9 +15,10 @@ class ProfilesController < ApplicationController
     begin 
       @profile = current_user.profile
       @profile.attributes = params[:profile]
-      @profile.save
+      @profile.save!
       redirect_to :action => 'index'
-    rescue
+    rescue Exception =>  e
+      logger.info "######################{e}"
       @profile.build_location if @profile.location.blank?
       redirect_to :action => 'edit'
     end
