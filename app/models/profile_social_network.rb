3e290535462f67ee56ca96fd16605ab785b05e07
pratik <pratik@joshsoftware.com>
2011-08-22 12:53:36 +0530

    ProfileSocilaNetwork and SocialNetwork
    Both the above model removed.There relation in profile model also
    removed

--- app/models/profile_social_network.rb
+++
@@ -1,4 +0,0 @@
-class ProfileSocialNetwork < ActiveRecord::Base
-  belongs_to :profile
-  belongs_to :social_network
-end
--- app/models/social_network.rb
+++
@@ -1,8 +0,0 @@
-class SocialNetwork < ActiveRecord::Base
-
-  has_many :profile_social_networks, :dependent => :destroy
-  has_many :profiles , :through => :profile_social_networks
-
-  validates :name , :presence => true, :uniqueness => true
-
-end
