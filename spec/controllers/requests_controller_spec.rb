ca07f3eaee0c8eb6148f5b01a42d05e6cb3c6a15
Amit Kulkarni <amit@joshsoftware.com>
2011-08-22 18:21:06 +0530

    Updated controllwer specs

--- spec/controllers/requests_controller_spec.rb
+++ spec/controllers/requests_controller_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 describe RequestsController do
-  context "Service request should be created if" do
+  context "Service request should be created by non logged in users if" do
     it "all the attributes are provided" do
       post :create, :request => {:title => "test-service-controller", :description => "Testing controller action for crrating request", :email => "testser@ser.com",
                                 :location_attributes => {:address => "nashik"}}
@@ -13,6 +13,30 @@ describe RequestsController do
     it "all the attributes are provided except address" do
       post :create, :request => {:title => "test-service-controller", :description => "Testing controller action for crrating request", :email => "testser@ser.com",
                                 :location_attributes => {:address => ""}}
+      req = Request.find_by_email("testser@ser.com")
+      req.should_not == nil
+      req.location.should == nil
+    end
+  end
+
+  context "Service request should be created by logged in users if" do
+    before(:each) do
+      @user = Factory(:user)
+      @request.env['devise.mapping'] = :user
+      sign_in @user
+    end
+    it "all the attributes are provided" do
+
+      post :create, :request => {:title => "test-service-controller", :description => "Testing controller action for crrating request", :email => "testser@ser.com",
+        :location_attributes => {:address => "nashik"}}
+      req = Request.find_by_email("testser@ser.com")
+      req.should_not == nil
+      req.location.should_not == nil
+    end
+
+    it "all the attributes are provided except address" do
+      post :create, :request => {:title => "test-service-controller", :description => "Testing controller action for crrating request", :email => "testser@ser.com",
+        :location_attributes => {:address => ""}}
       req = Request.find_by_email("testser@ser.com")
       req.should_not == nil
       req.location.should == nil
