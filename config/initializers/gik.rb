5eb56d7765e4a819b4cfefa5b3a1b54f1480b221
Sameer-Tilak <sameert@joshsoftware.com>
2011-08-22 11:30:22 +0530

    Set S3 location as per the rails env

--- config/initializers/gik.rb
+++ config/initializers/gik.rb
@@ -5,7 +5,7 @@ else
       :storage => :s3,
       :s3_credentials => "#{Rails.root}/config/s3.yml",
       :path => ":attachment/:id/:style.:extension",
-      :bucket => 'GIK_photos',                       
+      :bucket => "GIK_photos_#{Rails.env}",                       
       :default_url => "/images/missing/logo/:style.gif"
   }
 end
