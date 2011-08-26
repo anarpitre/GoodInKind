5c936f57541bf2d9a5b4092a2bdfc00f1feef295
Sameer <sameer@sameer-laptop.(none)>
2011-08-26 19:54:23 +0530

    Added code to increment/decrement nonprofit counter.

--- app/models/nonprofit_category.rb
+++ app/models/nonprofit_category.rb
@@ -4,4 +4,16 @@ class NonprofitCategory < ActiveRecord::Base
 
   validates :nonprofit_id , :presence => true
   validates :category_id, :presence => true
+
+  after_update { |nonprofit_categories|
+    change_category = nonprofit_categories.category_id_change
+    if(change_category)
+      if(change_category[0])
+        Category.decrement_counter(:nonprofit_count,change_category[0])
+       end
+       if(change_category[1])
+        Category.increment_counter(:nonprofit_count,change_category[1])
+       end
+    end
+  }
 end
