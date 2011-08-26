0050504bd3f3f47ad6a5057febed3e6750fff6ad
Sameer <sameer@sameer-laptop.(none)>
2011-08-26 09:28:27 +0530

    Changed page title i  noonprofit as per list given Removed column mission_statemant as per requirement Add image file types.

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -4,7 +4,7 @@ class Profile < ActiveRecord::Base
   
   accepts_nested_attributes_for :location, :allow_destroy => true
 
-  validates_attachment_content_type :avatar, :content_type => ["image/jpeg", "image/png", "image/gif", "image/jpg"]
+  validates_attachment_content_type :avatar, :content_type => ["image/jpeg", "image/png", "image/gif", "image/jpg", "image/bmp", "image/tiff"]
   validates_attachment_size  :avatar, :less_than => 2.megabytes
 
   # TBD - change size of medium as per HTML layout.
