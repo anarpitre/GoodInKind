ec544eca9ed512d41ae9c8d07670a9aac20b3694
Amit Kulkarni <amit@joshsoftware.com>
2011-08-29 18:17:14 +0530

    updated noncontroller specs

--- spec/factories/nonprofit.rb
+++ spec/factories/nonprofit.rb
@@ -13,7 +13,7 @@ Factory.define :nonprofit do |c|
   c.is_active true
   c.guideline {Faker::Lorem.paragraph}
   c.description {Faker::Lorem.paragraph}
-  c.phone_number {Faker::PhoneNumber.phone_number}
+  c.phone_number "1-222-333-4444"
   c.cell_phone "1234567890"
   c.association :gateway_id, :factory => 'gateway'
   c.photo_file_name "Rails.gif"
