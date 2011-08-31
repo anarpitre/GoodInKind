8e4d4408a576340abf2a67ae670a1ee17dba790c
Mehul Gurjar <mehul@joshsoftware.com>
2011-08-31 19:16:07 +0530

    + User Sign In (layout => signup) Used for it.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,29 @@
 class SessionsController < Devise::SessionsController
-  layout 'home'
+  before_filter :set_seo_tags
+  layout 'signup'
 
+  def new
+    @head[:title] = "SignIn"
+    @class_name = 'sign_up'
+    super
+  end
+  
   def create
+    @class_name = 'sign_up'
     session[:thank_you] = "yes" unless session[:service_id].blank?
     super
     change_service_offerer(session[:service_id],current_user.id) unless session[:service_id].blank? 
+  end
+
+  private
+
+
+  def set_seo_tags
+    @head = {
+      :title => "SignIn",
+      :keywords => "x y z",
+      :description => 'This really works'
+    }
   end
 
 
