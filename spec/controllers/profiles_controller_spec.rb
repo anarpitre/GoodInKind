737be9078b978e61459908e8fb37d47d4a90812a
Amit Kulkarni <amit@joshsoftware.com>
2011-09-03 12:56:39 +0530

    Added controller specs for controller

--- spec/controllers/profiles_controller_spec.rb
+++ spec/controllers/profiles_controller_spec.rb
@@ -32,6 +32,7 @@ describe ProfilesController do
                    :id => @user.id
       @user.reload
       @user.profile.avatar_file_name.should == "taj.jpg"
+      p @user.profile
     end
   end
 
@@ -42,6 +43,34 @@ describe ProfilesController do
                    :id => @user.id
       @user.reload
       @user.profile.avatar_file_name.should == nil
+    end
+    
+    it "image uploaded is in not in valid format i.e. .doc" do
+      file = File.open('spec/test.doc')
+      post :update,:profile => {:avatar => file, :about_me => "This is controller testing of image upload", :gender => "male", :last_name => "kulkarni", :first_name => "amit"},
+                   :id => @user.id
+      @user.reload
+      @user.profile.avatar_file_name.should_not == "test.doc"
+      p @user.profile
+      flash[:notice].should == "Please upload image in proer format"
+    end
+
+    it "image uploaded is not in valid format i.e. .pdf" do
+      file = File.open('spec/test.pdf')
+      post :update,:profile => {:avatar => file, :about_me => "This is controller testing of image upload", :gender => "male", :last_name => "kulkarni", :first_name => "amit"},
+                   :id => @user.id
+      @user.reload
+      @user.profile.avatar_file_name.should_not == "test.pdf"
+      flash[:notice].should == "Please upload image in proer format"
+    end
+    
+    it "image uploaded is not in valid format i.e. .txt" do
+      file = File.open('spec/test.txt')
+      post :update,:profile => {:avatar => file, :about_me => "This is controller testing of image upload", :gender => "male", :last_name => "kulkarni", :first_name => "amit"},
+                   :id => @user.id
+      @user.reload
+      @user.profile.avatar_file_name.should_not == "test.txt"
+      flash[:notice].should == "Please upload image in proer format"
     end
   end
 end
