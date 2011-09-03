25f0656a4f918465a77f63dae0e59cc374a27a33
Amit Kulkarni <amit@joshsoftware.com>
2011-09-03 12:57:17 +0530

    Updated specs for profile

--- spec/controllers/nonprofits_controller_spec.rb
+++ spec/controllers/nonprofits_controller_spec.rb
@@ -6,7 +6,8 @@ describe NonprofitsController do
   end
   context "Nonprofit should be" do
     it "created if all the attributes are valid with Post request and initial status should be Created" do
-      post :create, :nonprofit => {:name => "cry", :EIN => "123456", :website => "www.cry.com", :username => "non-cry", :password => "cry123", :password_confirmation => "cry123", :contact_name => "cry-user", :position => "manager", :email => "user@yser.com", :phone_number => "12345678", :cell_phone => "1234567890"}
+      file = File.open('spec/taj.jpg')
+      post :create, :nonprofit => {:name => "cry", :photo => file, :EIN => "12-1223456", :website => "www.cry.com", :username => "non-cry", :password => "cry123", :password_confirmation => "cry123", :contact_name => "cry-user", :position => "manager", :email => "user@yser.com", :phone_number => "123-456-1178", :cell_phone => "123-456-7890"}
       non_pro = Nonprofit.find_by_name("cry")
       non_pro.should_not == nil
       non_pro.is_verified.should == "created"
@@ -14,7 +15,7 @@ describe NonprofitsController do
 
     it "created if photo is uploaded along with other attributes" do
       file = File.open('spec/taj.jpg')
-      post :create, :nonprofit => {:name => "cry", :photo => file, :EIN => "123456", :website => "www.cry.com", :username => "non-cry", :password => "cry123", :password_confirmation => "cry123", :contact_name => "cry-user", :position => "manager", :email => "user@yser.com", :phone_number => "12345678", :cell_phone => "1234567890"}
+      post :create, :nonprofit => {:name => "cry", :photo => file, :EIN => "12-1213456", :website => "www.cry.com", :username => "non-cry", :password => "cry123", :password_confirmation => "cry123", :contact_name => "cry-user", :position => "manager", :email => "user@yser.com", :phone_number => "123-456-7118", :cell_phone => "123-456-7890"}
       non_pro = Nonprofit.find_by_name("cry")
       p non_pro
       non_pro.should_not == nil
@@ -22,7 +23,15 @@ describe NonprofitsController do
       non_pro.photo_file_name.should == "taj.jpg"
     end
 
-    it "update his guildeline and mission fields only if the status of nonprofit is verified"
+    it "update his guildeline and mission fields only if the status of nonprofit is verified" do
+      login_as @non_pro
+      p @non_pro.permalink
+      post :update, :id => @non_pro.permalink, :nonprofit =>{ :name => "non512", :location_attributes => {:address => "Pune", :id => "14"}, :category_ids => "14", :website => "www.qwwqq.com", :guideline => "testing", :description => "test"}
+     # post :update, :id=> @non_pro.permalink, 
+      #              :nonprofit => { :guideline => "This is testing guideline", :description => "This is testing description"}
+      non_pro = Nonprofit.find(@non_pro)
+      non_pro.guideline.should == "This is testing guideline"
+    end
 
     it "update his name and other profile fields only if the status of nonprofit is verified"
 
@@ -39,7 +48,7 @@ describe NonprofitsController do
     end
 
     it "able to get his username by clicking on forgot username link" do
-      post :forgot_username, :nonprofit => {:ein => @non_pro.EIN}
+      post :forgot_username, :nonprofit => {:EIN => @non_pro.EIN}
       flash[:notice].should == "Email was sent successfully"
     end
 
