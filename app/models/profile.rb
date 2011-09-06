6e806e8bd26673c61de3499e6c620922992e3f34
Gautam Rege <gautam@joshsoftware.com>
2011-09-06 23:58:35 +0530

    Adding helper methods to add profile from FB username

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -24,4 +24,22 @@ class Profile < ActiveRecord::Base
     "#{self.first_name} #{self.last_name}"
   end
 
+  def update_fb_image(username)
+    filename = "/tmp/#{username}.jpg"
+    temp = File.open(filename, 'wb') do |f|
+      f.write(FirstGiving.get("http://graph.facebook.com/#{username}/picture?type=large", :format => :binary).body)
+    end
+
+    File.open(filename, 'rb') do |f|
+      self.avatar = f
+    end
+
+    self.save!
+
+    File.unlink(filename)
+
+    rescue Exception
+      # Ignore all errors
+  end
+
 end
