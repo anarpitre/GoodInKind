6011a13f12e1e2fe456f6266f6e29612c73e5907
pratik <pratik@joshsoftware.com>
2011-09-06 21:14:22 +0530

    Validation removed for image size for service

--- app/models/image.rb
+++ app/models/image.rb
@@ -9,6 +9,6 @@ class Image < ActiveRecord::Base
   )
 
   validates_attachment_content_type :image, :content_type => ['image/jpeg', 'image/png','image/gif', 'image/jpg', 'image/bmp', 'image/tiff', 'image/tif']
-  validates_attachment_size :image, :less_than => 5.megabytes, :message => "Uploaded logo cannot be more than 5 MB"
+  #validates_attachment_size :image, :less_than => 5.megabytes, :message => "Uploaded logo cannot be more than 5 MB"
 
 end
