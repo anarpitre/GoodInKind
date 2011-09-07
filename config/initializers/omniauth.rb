a0ac2acd3ac91e72d6770309b3a497348ecff444
pratik <pratik@joshsoftware.com>
2011-09-07 13:25:49 +0530

    Social media configuration
    Configuration of social media application done in social_media.yml file
    and intialiased in social_media intialiser

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,23 +1,18 @@
 require 'omniauth/openid'
 require 'openid/store/filesystem'
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :twitter, 'qGccYRYVbwdH6tFlYah1Ng', 'vusGXcUm4GUGXq3qCOaKgjLNl0xulRB28Px0G3Bxrk'
-  provider :linked_in, 'a33rq3rqvlcs', 'x0JSZATLpG4uPkJQ'
-  if Rails.env =~ /development|test/
-    provider :facebook, FB_API_ID, FB_SECRET_KEY, {:client_options => {:ssl => {:ca_path => "/etc/ssl/certs"}}}
-  elsif Rails.env == 'staging'
-    provider :facebook, FB_STAGING_API_ID, FB_STAGING_SECRET_KEY, {:client_options => {:ssl => {:ca_path => "/etc/ssl/certs"}}}
-  elsif Rails.env == 'production'
-    provider :facebook, FB_PRODUCTION_API_ID, FB_PRODUCTION_SECRET_KEY, {:client_options => {:ssl => {:ca_path => "/etc/ssl/certs"}}}
-  end
-
-
-
+  
+  # User can Sign up only through facebook 
+  provider :facebook, FACEBOOK[:api_key], FACEBOOK[:secret_key], {:client_options => {:ssl => {:ca_path => "/etc/ssl/certs"}}}
+  
+  #provider :twitter, 'TWITTER[:api_key]', 'TWITTER[:secret_key]'
+  #provider :linked_in, 'LINKED_IN[:api_key]', 'LINKED_IN[:secret_key]'
+  
   # generic openid
-  provider :openid, OpenID::Store::Filesystem.new('./tmp'), :name => 'openid'
+  #provider :openid, OpenID::Store::Filesystem.new('./tmp'), :name => 'openid'
 
   # dedicated openid
-  provider :openid, OpenID::Store::Filesystem.new('./tmp'), :name => 'google', :identifier => 'https://www.google.com/accounts/o8/id'
+  #provider :openid, OpenID::Store::Filesystem.new('./tmp'), :name => 'google', :identifier => 'https://www.google.com/accounts/o8/id'
 
   #provider :google_apps, OpenID::Store::Filesystem.new('./tmp'), :name => 'google_apps'
   # /auth/google_apps; you can bypass the prompt for the domain with /auth/google_apps?domain=somedomain.com
