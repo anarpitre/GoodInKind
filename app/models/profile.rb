76323b2eab72ff16b1431b8fdf89b348b5ea7682
Sameer-Tilak <sameert@joshsoftware.com>
2011-09-07 18:30:26 +0530

    Fixed IE image uplaod issue

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -17,7 +17,9 @@ class Profile < ActiveRecord::Base
 
   validates_attachment_size :avatar, :less_than => 2.megabytes, :message => "file size should be less than 2MB"
 
-  validates_attachment_content_type :avatar, :content_type => ["image/jpeg", "image/png", "image/gif", "image/jpg", "image/bmp", "image/tiff", "image/tif","image/pjp    eg", "image/x-png" ], :message=> "Invalid image, image format should be jpeg/ jpg/ png/ gif"
+  validates_attachment_content_type :avatar, 
+    :content_type => ["image/jpeg", "image/png", "image/gif", "image/jpg", "image/bmp", "image/tiff", "image/tif","image/pjpeg", "image/x-png" ], 
+    :message=> "Invalid image, image format should be jpeg/ jpg/ png/ gif"
 
 
   def full_name
