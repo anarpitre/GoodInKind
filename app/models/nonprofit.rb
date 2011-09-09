c24cf6c715312bc7b8eb0a84d701aef7d1889f13
Sameer-Tilak <sameert@joshsoftware.com>
2011-09-09 21:48:09 +0530

    Reverted image change.

--- app/models/nonprofit.rb
+++ app/models/nonprofit.rb
@@ -33,8 +33,8 @@ class Nonprofit < ActiveRecord::Base
 
   has_attached_file :photo, S3_DEFAULTS.merge(
     :styles => {
-    :thumb => ["172x62>", :jpg],
-    :medium => ["200x61>", :jpg]
+    :thumb => "172x62>",
+    :medium => "200x61>"
     },
 
     :convert_options => { :all => '-strip -colorspace RGB'},
