195a6f9bb7d47a3f94bde23ec92b6aa170dda561
Amit Kulkarni <amit@joshsoftware.com>
2011-09-13 17:12:34 +0530

    update for integrity

--- config/boot.rb
+++ config/boot.rb
@@ -6,7 +6,7 @@ require 'rubygems'
 #require 'bundler/setup' if File.exists?(ENV['BUNDLE_GEMFILE'])
 
 # Set up gems listed in the Gemfile.
-GEMFILE_PATH = File.expand_path('../../Gemfile', __FILE__)
+GEMFILE_PATH = File.expand_path('../../Gemfile', File.dirname(__FILE__))
 if File.exist?(GEMFILE_PATH)
   # Force the rails 3 application to use its Gemfile
   ENV['BUNDLE_GEMFILE'] = GEMFILE_PATH
